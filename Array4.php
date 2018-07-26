45c0133eb48ea7ef2112f36214f554229644ca93
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:42:31 +0530

    2D array examples

---
+++ Array4.php
@@ -0,0 +1,12 @@
+ <?php 
+  $Array[0][0]=10;
+   $Array[0][]=11;
+   $Array[0][]=12;
+   $Array[]['shahnawaz']=13;
+   $Array[0][]=14;
+   $Array[0][]=15;
+   $Array[1][]=13;
+   echo "<pre>";
+   print_r($Array);
+   echo "</pre>";
+   ?>
---
+++ Array5.php
@@ -0,0 +1,11 @@
+<?php
+  $Array=array();
+  $Array[0]=array(1,2,3,4,5);
+  
+  $Array[]=array(1,2,3,4,5);
+  $Array["shahnawaz"]=array(1,2,3,4,5);
+  echo "<pre>";
+   print_r($Array);
+   echo "</pre>";
+
+?>
