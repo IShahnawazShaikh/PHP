69863ef054a59bf803c6f86db4bf425f79b7c087
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:38:24 +0530

    UnderStanding Operator in PHP

---
+++ Oprator.php
@@ -0,0 +1,34 @@
+<?php
+ $str1="shahnawaz";
+ $str2="shaikh";
+ echo $str1." ".$str2;     //concatenation operator
+  $x=10;
+   $y=20;
+echo "<br>Sum is:".($x+$y);
+echo "<br>Mod is:".($x%$y);
+$x++;
+ECHO "<br>".$x;   // increment operator
+if($y>$x){
+   echo "<br> $y is greater than $x";
+}
+ else{
+   echo "<br> $y is smaller than $x";
+}
+echo "<br>";
+ $p=5;
+$q='5';
+if($p==$q){
+      echo "Both are equal";
+} 
+else{
+    echo "Not equal";
+}
+echo"<br>";
+if($p===$q){
+      echo "Both are equal";
+} 
+else{
+    echo "Not equal";
+}
+
+?>
