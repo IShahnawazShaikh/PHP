2640dbf7706acc6c998b2ff84c5694bdf57caf99
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:38:55 +0530

    onditionalOPerator in php

---
+++ ConditionalOperator.php
@@ -0,0 +1,14 @@
+<html> 
+<head><title>Arithmetical Operators</title><head> 
+<body> 
+<?php 
+    $a = 40; 
+    $b = 55; 
+     
+    $result = ($a > $b ) ? $a :$b; 
+    echo "Value of result is $result<br/>";  
+    $result = ($a < $b ) ? $a :$b; 
+    echo "Value of result is $result<br/>"; 
+?> 
+</body> 
+</html> 
