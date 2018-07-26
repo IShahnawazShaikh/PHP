aea26401835222fc294a7fee9cef5a9c58c719a6
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:43:56 +0530

    IFElse in php

---
+++ IfElse.php
@@ -0,0 +1,24 @@
+<?php
+
+$x="10";
+//if-else
+if($x===10){
+	echo "If is excecuted";
+}
+else{
+	echo "Else is executed"."<br>";
+}
+//if elseif if
+ $z=10;
+ if($z>11){
+	 echo "If executed";
+ }
+ elseif($z>12){
+	 echo "else if executed";
+ }
+ else{                                               //Run only 1 block out of three cases
+    echo "else is executed";
+ }
+
+
+?>
