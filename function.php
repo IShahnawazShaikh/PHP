3382fe7baa51678a7b4df60c31c4f7916b4eecce
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:43:36 +0530

    Function in php

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+
+   function myFunction($value){
+        echo "value ".$value."<br>";
+   }  
+    myFunction('shahnawaz');          //calling funtion
+?>
+<html> 
+<head>
+<title>Funtion</title>
+</head>
+<body>
+   <?php 
+   myFunction(10);     //calling funtion from html
+   ?>     
+</body
+
+</html>
---
+++ function2.php
@@ -0,0 +1,7 @@
+<?php 
+ function myFunction($a,$b){
+	 return $a+$b;
+ }
+ $Answer=myFunction(2,3);
+ echo "Answer=$Answer";
+ ?>
