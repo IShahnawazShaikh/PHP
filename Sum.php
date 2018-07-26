66434237ae6ee64a5056c9ebb090498d0c59871b
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:37:37 +0530

    Addition in pHP

---
+++ Sum.php
@@ -0,0 +1,12 @@
+<?php
+   echo "<h1><h2>Sum Of two number</h2>Using Variables</h1>";
+    $x=5;   //declaring variable n assign
+    $y=10;
+     $z=$x+$y;
+    echo "<br>Answer is:$z";
+    print "<br>";
+   echo $z;
+echo "<br>";
+  echo  $x+$y;
+  
+?>
