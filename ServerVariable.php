dcc131b3850f860fb2419cd621b5880e879d07c3
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:47:03 +0530

    Server Variable
    This is a Superglobal array

---
+++ ServerVariable.php
@@ -0,0 +1,8 @@
+<?php
+echo $_SERVER['SERVER_NAME'];
+
+echo $_SERVER['PHP_SELF']."<br>";
+//echo $_SERVER['HTTP_HOST']."<br>";
+echo $_SERVER['HTTP_USER_AGENT']."<br>";
+echo $_SERVER['REMOTE_ADDR'];
+?>
