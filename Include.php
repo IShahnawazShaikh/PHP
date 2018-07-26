437c41c50ed029ddd307a3af48909b0b800c5faf
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:44:28 +0530

    Understanding Include and Require

---
+++ Include.php
@@ -0,0 +1,14 @@
+<?php include("path.php"); ?>
+<html>
+  <head>
+  <title>InludeStatement</title>
+  </head>
+  <body>
+     <h4>We ARE GOIING TO UNDERSTAND INCLUDE STATEMENTS</h4>
+	 <span>Calculating Area of rectangle</span>
+     <?php 
+	    $Result=Area(12,12);
+		echo "<br>Area of rectange is=".$Result;
+	 ?>	 
+  </body>
+</html>
