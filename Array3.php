1f2ef62f70749ab04165f6d8810f44e5fd08195e
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:41:35 +0530

    2D Array in PHP

---
+++ Array3.php
@@ -0,0 +1,17 @@
+<?php
+   //Two dimentional Array
+   $Array=array();
+   $Array[][]=10;
+   $Array[][]=11;
+   $Array[][]=12;
+   $Array[]['shahnawaz']=13;
+   $Array['s'][]=14;
+   $Array[][]=15;
+   
+   
+   
+   
+    echo "<pre>"; 
+     print_r($Array);
+	 echo "</pre>";
+?>
