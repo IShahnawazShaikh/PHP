048b643320c2df1c9d6a059de3ff50faaceaca0c
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-07-26 22:39:10 +0530

    First Program in Php

---
+++ first.php
@@ -0,0 +1,11 @@
+<html>
+ <head></head>
+ <body>
+  <p>This is PHP part</p>
+ <?php
+     //Single line comment
+    print "<h1>Html  H1 tag in PHP</p<br>";
+   echo "<p>Html  P tag in PHP</p";
+   ?>
+</body>
+</html>
